7c828548269ce3ba6422b0936f9b90d3246697e7
electrocodeur <77399487+electrocodeur@users.noreply.github.com>
2022-07-20 21:50:44 +0200

    Add files via upload

---
+++ sensorgaz_stm32.ino
@@ -0,0 +1,16 @@
+int sensor = PB10;
+void setup() {
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(sensor) == HIGH) {
+    Serial.println("etat haut");
+    delay(100);
+  }
+  if (digitalRead(sensor) == LOW) {
+    Serial.println("etat bas");
+    delay(100);
+  }
+}
